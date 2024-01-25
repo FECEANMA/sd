ed29685ad1a64e17008c2a257be7342870adbe86
Fernando <146852179+FECEANMA@users.noreply.github.com>
2024-01-25 09:54:58 -0500

    Add files via upload

---
+++ 5.10/cuarto.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let arr = ["Juan", "Smith"]
+
+// asignación desestructurante
+// fija firstName = arr[0]
+// y surname = arr[1]
+let [firstName, surname] = arr;
+
+alert(firstName); // Juan
+alert(surname);  // Smith
